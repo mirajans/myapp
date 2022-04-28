234d2acacd526cd233d51a6455171a40c4319bf5
mirajans <miraj.ansari@sysfore.com>
2022-04-28 16:27:04 +0530

    add todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './myComponent/todos/todos.component';
+import { TodoItemComponent } from './myComponent/todo-item/todo-item.component';
+import { AddTodoComponent } from './myComponent/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
